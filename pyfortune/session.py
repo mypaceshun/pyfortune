f9b9a9d490e2c3d7a414f9b2a6ee7fd605e281b1
Shun Kawai <shun@osstech.co.jp>
2019-03-14 13:05:48 +0900

    docstringsを書いた

--- pyfortune/session.py
+++ pyfortune/session.py
@@ -6,16 +6,38 @@ LOGIN = 'login'
 LOGOUT = 'logout'
 
 class Session():
+    """ログインセッションを張るためのクラス
+    """
     def __init__(self):
+        """初期化処理を行う
+        """
         self.cookies = None
         self.BASE_URL = 'https://fortunemusic.jp/'
         self.LOGIN_URL = '{}default/login/'.format(self.BASE_URL)
 
     def _pre_cookie(self):
+        """事前にTOPページにアクセスし、Cookieを取得する
+        """
         res = requests.get(self.BASE_URL)
         self.cookies = res.cookies
 
     def login(self, username, password):
+        """ログイン処理を行う関数
+
+        Args:
+            username (str): ユーザー名
+            password (str): パスワード
+
+        Returns:
+            str: ログインに成功したユーザー名
+
+        Raises:
+            pyfortune.exception.LoginfailureException
+
+        Examples:
+            >>> login('username', 'password')
+            'username'
+        """
         self._pre_cookie()
         data = {'login_id': username,
                 'login_pw': password}
@@ -28,6 +50,15 @@ class Session():
         return username
 
     def status(self):
+        """ログインチェックを行う関数
+
+        Returns:
+            str: `pyfortune.session.LOGIN`か`pyfortune.session.LOGOUT`が返される
+
+        Examples:
+            >>> status()
+            'login'
+        """
         res = requests.get(self.BASE_URL,
                     cookies=self.cookies)
         soup = BeautifulSoup(res.text, 'html.parser')
