51bd18bca2e5dc4f516d686383b5e476a896229d
konishi0125 <yumemita@gmail.com>
2019-07-26 23:35:50 +0900

    過去に行ったことあるメンバー一覧とその枚数取得　グループ複数検索対応

--- pyfortune/test_search.py
+++ pyfortune/test_search.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 import search
 
-s = search.Search("username", "pass", group = "日向坂", number = None) 
+s = search.Search("user", "pass", group = ["欅坂", "日向坂"], number = None) 
 data = s.search(member = "潮　紗理菜") #性、名の間全角スペース 
-print(data)
+#print(data)
 
 sum_ = s.sum_sheets("潮　紗理菜")
-print(sum_)
-=======
-import search
+#print(sum_)
 
-s = search.Search("user", "pass", group="日向坂", number="2")
-data = s.search(member="潮　紗理菜")  # 性、名の間全角スペース
-print(data)
->>>>>>> 9faf507eb2d0983d0ba38cf844f38cd5834ca106
+output = s.past_member()
+print(output)
